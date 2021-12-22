c86746cb1f6c78eaa96ce1b6faab73a870a9f2a8
Frank Yu <frank@yuyuan.org>
2021-12-22 01:42:12 -0800

    Update!

--- forge-1.18.1-39.0.7-mdk/src/main/java/com/example/mymod/setup/Registration.java
+++ forge-1.18.1-39.0.7-mdk/src/main/java/com/example/mymod/setup/Registration.java
@@ -1,17 +1,22 @@
 package com.example.mymod.setup;
 
 import com.example.mymod.MyMod;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public class Registration {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MyMod.MOD_ID);
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.BLOCKS, MyMod.MOD_ID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MyMod.MOD_ID);
 
     public static void register() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         BLOCKS.register(modEventBus);
         ITEMS.register(modEventBus);
+
+        ModItems.register();
     }
 }
