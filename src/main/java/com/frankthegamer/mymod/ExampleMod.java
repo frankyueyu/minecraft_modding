48f9144edae3bc5d36d2c36e9ca9de6156c9ee01
Frank Yu <frank@yuyuan.org>
2022-01-30 01:41:31 -0800

    I changed some things

--- src/main/java/com/frankthegamer/mymod/ExampleMod.java
+++ src/main/java/com/frankthegamer/mymod/ExampleMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.frankthegamer.mymod;
 
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -18,7 +18,7 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
+@Mod("mymod")
 public class ExampleMod
 {
     // Directly reference a log4j logger.
